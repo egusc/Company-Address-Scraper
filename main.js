66ea5060cd0bd0070a8d72b42f0d7a548b9b19e5
Ewan Guscott <e.guscott@lancaster.ac.uk>
2023-08-10 11:50:48 +0100

    Attempts HTTPS request

--- main.js
+++ main.js
@@ -2,15 +2,34 @@
 const cheerio = require("cheerio");
 const Knwl = require("knwl.js");
 const prompt = require("prompt-sync")();
+const https = require('node:https');
+
+
+const httpdoc = [];     //Array storing HTTP code
+
+//EXAMPLE CODE - TRIES TO GET HTML FROM WEBSITE
+//Here is where I encounter 403 errors
+https.get('https://www.bbc.co.uk/', (res) => {
+
+  res.on('data', (d) => {
+    httpdoc.push(d);
+  });
+  res.on('end', (d) => {
+    console.log(httpdoc.join());
+  });
+}).on('error', (e) => {
+  console.error(e);
+}); 
+
 
 const blockedDomains = ["hotmail", "gmail", "outlook", "aol", "proton", "yahoo", "icloud"]; //Array of personal email providers to block
 
 //Promps user to input email and extracts domain name
-var inputEmail = prompt("Please enter the email that you would like to find the company of: ");
-var indexOfAt = inputEmail.indexOf("@");
-var indexOfDomainEnd = inputEmail.indexOf(".", indexOfAt);
-var domain = inputEmail.slice(indexOfAt + 1, indexOfDomainEnd);
-var domainSite = inputEmail.slice(indexOfAt + 1);
+let inputEmail = prompt("Please enter the email that you would like to find the company of: ");
+let indexOfAt = inputEmail.indexOf("@");
+let indexOfDomainEnd = inputEmail.indexOf(".", indexOfAt);
+let domain = inputEmail.slice(indexOfAt + 1, indexOfDomainEnd);
+let domainSite = inputEmail.slice(indexOfAt + 1);
 
 //Verifies that email isn't personal email based on blocked domains
 if(blockedDomains.includes(domain))
